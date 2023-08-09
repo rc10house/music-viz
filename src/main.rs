895090cf90eb074b573bfe85d059bf438dc13db7
rthenhaus <rthenhaus>
2023-08-09 11:45:02 -0500

    Added download from youtube

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use ngrammatic::{CorpusBuilder, Pad};
-use reqwest::{self, Response};
+use reqwest::{self};
 use rodio::{Decoder, OutputStream, Sink};
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io::{stdin, BufReader};
 use std::path::PathBuf;
+use std::process::Command;
 
 const YOUTUBE_SEARCH: &str =
     "https://youtube.googleapis.com/youtube/v3/search?part=snippet&part=id&q=";
 const API_KEY: &str = "&key=AIzaSyCIVk8DHCPmnOEuTbxol_S_a9DeJcVQM68";
+const DOWNLOAD_LOC: &str = "/Users/Ryan/soft/music-viz/library/";
+const YOUTUBE_URL: &str = "https://www.youtube.com/watch?v=";
 
 // Searches the library for a song name using fuzzy search
-fn search_library(library: &HashMap<String, PathBuf>) -> PathBuf {
+fn search_library(library: &mut HashMap<String, PathBuf>) -> PathBuf {
+    load_library(library);
+
     // build corpus for searching
     let mut corpus = CorpusBuilder::new().arity(2).pad_full(Pad::Auto).finish();
 
     // Aggregate names of songs into corpus
-    for (key, _) in library {
+    for (key, _) in &mut *library {
         corpus.add_text(key);
     }
 
@@ -32,7 +36,6 @@ fn search_library(library: &HashMap<String, PathBuf>) -> PathBuf {
             println!("Play this song? (y/n)");
             input.clear();
             stdin().read_line(&mut input).expect("[!] Invalid input");
-            println!("{}", input);
             match input.as_str().trim_end() {
                 "y" => {
                     return library
@@ -58,7 +61,7 @@ fn search_library(library: &HashMap<String, PathBuf>) -> PathBuf {
 }
 
 // Plays a song
-fn play_song(library: &HashMap<String, PathBuf>) {
+fn play_song(library: &mut HashMap<String, PathBuf>) {
     let song: PathBuf = search_library(library);
     // get output stream handle to sound device
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
@@ -89,39 +92,78 @@ fn load_library(library: &mut HashMap<String, PathBuf>) {
         name.truncate(name.len() - 4);
         library.insert(name, path);
     }
-    println!("HashMap: {:?}", library);
 }
 
 // Displays the library for the user
-fn show_library(library: &HashMap<String, PathBuf>) {
+fn show_library(library: &mut HashMap<String, PathBuf>) {
+    load_library(library);
+
     for (key, _) in library {
         println!("{key}");
     }
 }
 
-fn send_request(search_string: String) {
+// Sends search request to youtube api and returns user-picked result
+fn send_request(search_string: String) -> String {
     // format search string
     let formatted_string = search_string.replace(" ", "%20");
+
+    println!("Searching...");
 
     let body = reqwest::blocking::get(YOUTUBE_SEARCH.to_owned() + &formatted_string + API_KEY)
         .expect("[!] request faileld")
         .json::<serde_json::Value>()
         .expect("[!] Reading response failed");
 
+    let mut id_vec = Vec::new();
+
     for i in 0..5 {
-        println!(
-            "{} - {} - {}",
-            i, body["items"][i]["snippet"]["title"], body["items"][i]["id"]["videoId"]
-        );
+        id_vec.push(body["items"][i]["id"]["videoId"].clone().to_string());
+        println!("{} - {}", i + 1, body["items"][i]["snippet"]["title"]);
     }
+
+    println!("\nSelect 1-5");
+    let mut input = String::new();
+    let mut choice: usize;
+
+    loop {
+        input.clear();
+        stdin().read_line(&mut input).expect("[!] Invalid input");
+        choice = input.as_str().trim_end().parse().unwrap();
+
+        if choice > 5 || choice < 1 {
+            println!("Invalid number, try again");
+            continue;
+        }
+        break;
+    }
+
+    // minus 1 to reflect real index
+    return id_vec[choice - 1].clone();
 }
 
 // Pulls song from youtube in mp3 form and adds it to library
-fn download_song(library: &mut HashMap<String, PathBuf>) {
+fn download_song() {
     let mut input = String::new();
     println!("Search Youtube:");
     stdin().read_line(&mut input).expect("[!] Invalid input");
-    send_request(input);
+    // search
+    let song_id: String = send_request(input).replace("\"", "");
+    let mut input = String::new();
+
+    println!("Set song name:");
+    stdin().read_line(&mut input).expect("[!] Invalid input");
+
+    println!("Downloading...");
+    Command::new("yt-dlp")
+        .arg(YOUTUBE_URL.to_owned() + &song_id)
+        .arg("-x")
+        .arg("--audio-format")
+        .arg("mp3")
+        .arg("--output")
+        .arg(DOWNLOAD_LOC.to_owned() + &input.trim_end() + ".mp3")
+        .spawn()
+        .expect("[!] yt-dlp failed to execute");
 }
 
 fn main() {
@@ -145,9 +187,9 @@ fn main() {
         stdin().read_line(&mut input).expect("[!] Invalid input");
 
         match input.as_str().trim_end() {
-            "1" => play_song(&library),
-            "2" => show_library(&library),
-            "3" => download_song(&mut library),
+            "1" => play_song(&mut library),
+            "2" => show_library(&mut library),
+            "3" => download_song(),
             "4" => println!("{}", welcome_string),
             "5" => break,
             _ => println!("[!] Invalid option"),
